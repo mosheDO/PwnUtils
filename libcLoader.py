2f2059cb98958de8b19e57d8c760699e731f0e99
moshe <moshedoppelt@gmail.com>
2024-04-10 17:57:43 +0300

    add pwninit usage

--- libcLoader.py
+++ libcLoader.py
@@ -12,6 +12,7 @@ import argparse
 
 
 ARCH_LIST = ['amd64', 'i386']
+LIBC_NAME = "libc.so.6"
 LIBC_LOCATION = "lib/x86_64-linux-gnu/libc.so.6"
 URL_TEMPLATE = "https://launchpad.net/ubuntu/+archive/primary/+files/libc6_{version}-0ubuntu{seqnum}_{arch_base}.deb"
 
@@ -34,7 +35,7 @@ def download_file(version_number, seqnum, arch_base):
     response = requests.get(url, stream=True)
     total_size = int(response.headers.get('content-length', 0))
     if total_size < 1024 * 1024:  # Check if file size is below 1 MB
-        print("[-] \033[91mDownloaded file size is below 1 MB. Skipping...\033[0m")
+        print("[-] \033[91mSkipping download: File size is below 1 MB and does not match the expected size for the libc file.\033[0m")
         return None
     block_size = 1024
     progress_bar = tqdm(total=total_size, unit='B', unit_scale=True)
@@ -56,11 +57,23 @@ def extract_deb(deb_file, extract_dir):
     subprocess.run(["dpkg", "-x", deb_file, extract_dir])
 
 
+def call_pwninit(bin_path, libc_path, ld_path=None):
+    command = ["pwninit", "--bin", bin_path, "--libc", libc_path]
+    if ld_path:
+        command.extend(["--ld", ld_path])
+    result = subprocess.run(command, capture_output=True, text=True)
+    if result.returncode == 0:
+        print("[+] \033[92mpwninit ran successfully! Ready to pwn!\033[0m")
+    else:
+        print("[-] \033[91mpwninit failed to run.\033[0m")
+
+
 def main():
     parser = argparse.ArgumentParser(description='Download libc6 package for a specified version from the Ubuntu launchpad repository.')
     parser.add_argument('version_number', metavar='VERSION', type=str, nargs='?', help='The version number of the libc6 package')
     parser.add_argument('-a', '--arch',  type=str, nargs='?', help='The architecture of the libc6 package. If not provided,  Resolve architecture automatically.')
-    
+    parser.add_argument('-b', '--binary', type=str, help='Path to the binary file. Used to pwninit (if needed)')
+
     args = parser.parse_args()
     
     if args.version_number is None or args.version_number == '-h':
@@ -95,6 +108,15 @@ def main():
                 print(f"[+] \033[92mDeleted {deb_file}\033[0m")
                 break
         seqnum += 1
+    if args.binary:
+        print(f"[+] \033[92mUsing pwninit with binary: {args.binary}, and downloaded libc'\033[0m")
+        # Assuming LIBC_NAME is defined somewhere
+        if os.path.exists(LIBC_NAME) and os.path.exists(args.binary):
+            call_pwninit(bin_path=args.binary, libc_path=LIBC_NAME)
+        else:
+            print(f"[-] \033[91mError: {LIBC_NAME} does not exist\033[0m")
+    else:
+        print("[-] \033[93mNo binary file provided. Use -b/--binary to specify the binary file path\033[0m")
 
 
 if __name__ == "__main__":
