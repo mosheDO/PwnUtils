160cffd77d5bea725879279124f9d0fb234a63cb
moshe <moshedoppelt@gmail.com>
2024-05-31 18:36:55 +0300

    Update solve.py

--- solve.py
+++ solve.py
@@ -154,11 +154,11 @@ sl = lambda *args, **kwargs: None
 ru = lambda *args, **kwargs: None
 def shortcut(io):
     global sa
-    sa = lambda x,y : io.sendafter(x, str(y))
+    sa = lambda x,y : io.sendafter(x, y)
     global sla
-    sla = lambda x,y : io.sendlineafter(x, str(y))
+    sla = lambda x,y : io.sendlineafter(x, y)
     global sl
-    sl = lambda x : io.sendline(str(x))
+    sl = lambda x : io.sendline(x)
     global ru
     ru = lambda x : io.readuntil(x)
 
